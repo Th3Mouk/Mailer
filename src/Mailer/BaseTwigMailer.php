35a92a03c053f8762407c81615ad274870555ace
Jérémy Marodon <marodon.jeremy@gmail.com>
2015-11-19 09:46:42 +0100

    [ADD] New render method more reusable

--- src/Mailer/BaseTwigMailer.php
+++ src/Mailer/BaseTwigMailer.php
@@ -19,13 +19,34 @@ class BaseTwigMailer extends BaseMailer
      */
     public function __construct(\Swift_Mailer $mailer, EngineInterface $templating)
     {
-        parent::__construct($mailer);
         $this->templating = $templating;
+        parent::__construct($mailer);
     }
 
+    /**
+     * Function to draw the body.
+     *
+     * @deprecated
+     *
+     * @param $template
+     * @param $datas
+     */
     public function renderHTMLBody($template, $datas)
     {
-        $this->setBody($this->templating->render($template, array("contact" => $datas)));
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.1 and will be removed in 2.0.', E_USER_DEPRECATED);
+        $this->setBody($this->templating->render($template, array('contact' => $datas)));
+        $this->setContentType('text/html');
+    }
+
+    /**
+     * Function to set the body of the mail with HTML template.
+     *
+     * @param $template
+     * @param $parameters
+     */
+    public function setHTMLBody($template, $parameters)
+    {
+        $this->setBody($this->templating->render($template, $parameters));
         $this->setContentType('text/html');
     }
 }
